216642ab3eac5a49efa133c132175d03e0ef92da
dannycyph3r <47256908+dannycyph3r@users.noreply.github.com>
2019-02-21 22:04:37 -0500

    4

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-let list = document.getElementsByTagName("LI");
+
+let myNodelist = document.getElementsByTagName("LI");
 let i;
 for (i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
+
 let close = document.getElementsByClassName("close");
-let i;
+var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
@@ -18,11 +20,10 @@ for (i = 0; i < close.length; i++) {
 }
 
 
-let list = document.querySelector('ul');
+var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
-
 
